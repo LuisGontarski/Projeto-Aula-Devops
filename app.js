8c9012a1a19b6376d3345a899ab8801d65eb4f4a
Luis Gontarski <83317182+LuisGontarski@users.noreply.github.com>
2024-10-18 19:42:21 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const sql = require('mssql');
+
 const app = express();
 
 // Middleware para analisar JSON no corpo da requisição
@@ -14,11 +15,11 @@ const dbConfig = {
     database: process.env.SQL_DATABASE,
     options: {
         encrypt: true,
-        trustServerCertificate: false
+        trustServerCertificate: true // Mude para 'false' em produção
     }
 };
 
-// URL da Azure Function e Microsserviço
+// URLs das APIs
 const AZURE_FUNCTION_URL = 'https://7n2b14zsc7.execute-api.us-east-2.amazonaws.com/lambda';
 const MICROSERVICE_URL = 'https://crud-croud.internal.wonderfulsmoke-a37fd7b3.australiaeast.azurecontainerapps.io';
 
@@ -38,16 +39,17 @@ app.get('/api/reservas', async (req, res) => {
         const result = await sql.query`SELECT * FROM reservas`;
         res.json(result.recordset);
     } catch (error) {
+        console.error('Erro ao buscar reservas:', error);
         res.status(500).json({ error: 'Erro ao buscar reservas' });
     }
 });
 
-// Rota do BFF para fazer requisições para o microsserviço e Azure Function
+// Rota do BFF para agregar dados de microsserviço e Azure Function
 app.get('/api/bff', async (req, res) => {
     try {
         // Requisição para o microsserviço
         const microserviceResponse = await axios.get(`${MICROSERVICE_URL}/reservas`);
-        
+
         // Requisição para a Azure Function
         const azureFunctionResponse = await axios.get(AZURE_FUNCTION_URL);
 
